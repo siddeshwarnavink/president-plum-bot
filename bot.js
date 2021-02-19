f87f2a3626caea588a2da1af448b03f87606492f
Siddeshwar <siddeshwar.work@gmail.com>
2021-02-19 08:33:36 +0530

    added greeting message

--- bot.js
+++ bot.js
@@ -16,4 +16,17 @@ client.on('message', msg => {
 	}
 });
 
+client.on('guildMemberAdd', member => {
+	const channel = member.guild.channels.cache.find(ch => ch.id === process.env.WELCOME_CHANNEL_ID);
+  	
+	if (!channel) return;	
+
+	console.log(member);
+
+	channel.send(`Welcome to the server, ${member}`);
+	channel.send('Hope you have a great time here!');
+
+	member.roles.set([process.env.NEWBIE_ROLE_ID]);
+});
+
 client.login(process.env.BOT_TOKEN);
